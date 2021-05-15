ebb849fd144fd2e3f4c6a33aa6e220a29f1f58f0
Neil Kaushikkar <neil.kaushikkar@gmail.com>
2021-05-15 18:29:07 -0500

    Add classification functionality

--- src/naivebayes/gaussian_classification.rs
+++ src/naivebayes/gaussian_classification.rs
@@ -1,26 +1,71 @@
 extern crate num_traits;
 
-use crate::naivebayes::gaussian_feature::GaussianFeature;
-use crate::naivebayes::classification::Classification;
 use self::num_traits::ToPrimitive;
+use std::f64::consts::PI;
 use std::vec::Vec;
 
-pub struct GaussianClassification<T> {
-    labels: Vec<T>,
-    features: Vec<GaussianFeature>
+#[derive(Debug, Default)]
+pub struct GaussianClassification {
+    mean: f64,
+    std: f64
 }
 
-impl<T> GaussianClassification<T> {
-    pub(crate) fn get_class_index(&self, class: &T) -> usize {
-        0
+fn mean<Num: ToPrimitive + Copy>(features: &Vec<Num>) -> f64 {
+    features.iter().enumerate().fold(0.0, |avg, (i, &x)|
+        {
+            let value: f64 = match x.to_f64() {
+                None => avg,
+                Some(n) => n
+            };
+
+            avg + ((value - avg) / (i + 1) as f64)
+        })
+}
+
+fn std<Num: ToPrimitive + Copy>(features: &Vec<Num>) -> f64 {
+    let mut sample_size: usize = features.len();
+    let mean: f64 = mean(features);
+
+    let sum: f64 = features
+        .iter()
+        .map(|x: &Num| {
+            match x.to_f64() {
+                None => {
+                    sample_size -= 1;
+                    0.0
+                }
+                Some(n) => (n - mean).powf(2.0)
+            }
+        })
+        .sum::<f64>();
+
+    (sum / sample_size as f64).sqrt()
+}
+
+impl GaussianClassification {
+    pub(crate) fn new<Num: ToPrimitive + Copy>(features: Vec<Num>) -> GaussianClassification {
+        GaussianClassification {
+            mean: mean(&features),
+            std: std(&features)
+        }
     }
-}
 
-impl<T> Classification<T> for GaussianClassification<T> {
-    fn feature_likelihood_given_class<Num: ToPrimitive + Copy>(&self, sample_feature: Num, class: &T) -> f64 {
-        let class_idx: usize = self.get_class_index(class);
-        let model_feature: &GaussianFeature = &self.features[class_idx];
+    pub(crate) fn create<Num: ToPrimitive + Copy>(mean: f64, std: f64) -> GaussianClassification {
+        GaussianClassification {
+            mean,
+            std
+        }
+    }
 
-        model_feature.pdf(sample_feature)
+    pub(crate) fn pdf<Num: ToPrimitive>(&self, x: Num) -> f64 {
+        match x.to_f64() {
+            None => 0.0,
+            Some(n) => {
+                let multiplier: f64 = 1.0 / (self.std.powf(2.0) * 2.0 * PI);
+                let exponent: f64 = -0.5 * ((n - self.mean) / self.std).powf(2.0);
+
+                multiplier * exponent.exp()
+            }
+        }
     }
 }

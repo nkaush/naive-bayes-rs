b8acf5e47545e5aafd64470c765b81bb15bf40aa
Neil Kaushikkar <neil.kaushikkar@gmail.com>
2021-05-21 20:20:05 -0700

    Add minor tweaks to cli argument handling

--- src/main.rs
+++ src/main.rs
@@ -11,12 +11,11 @@ use clap::{App, load_yaml};
 use std::string::String;
 use ml::model::Model;
 
-
 fn run_app() -> Result<(), String> {
     let yaml = load_yaml!("cli-arguments.yaml");
     let arg_matches = App::from(yaml).get_matches();
 
-    // println!("{:?}", arg_matches);
+    // TODO verbosity, confusion
 
     let labels_path: Option<String> = match arg_matches.value_of("labels") {
         Some(label_path) => Some(String::from(label_path)),
@@ -70,31 +69,6 @@ fn run_app() -> Result<(), String> {
     }; 
 
     Ok(())
-
-    // let label_path: String = String::from("./data/digits/emnist-digits-mapping.txt");
-    // let train_path: String = String::from("./data/digits/emnist-digits-train.csv");
-    // let test_path: String = String::from("./data/digits/emnist-digits-test.csv");
-
-    // let label_path: String = String::from("./data/balanced/emnist-balanced-mapping.txt");
-    // let train_path: String = String::from("./data/balanced/emnist-balanced-train.csv");
-    // let test_path: String = String::from("./data/balanced/emnist-balanced-test.csv");
-
-    // let balanced_path: String = String::from("./models/balanced/balanced.json");
-
-    // let model: &mut GaussianNaiveBayes = &mut GaussianNaiveBayes::from_labels(&label_path);
-    // let model: &mut GaussianNaiveBayes = &mut GaussianNaiveBayes::from_json(&balanced_path);
-
-    // match model.train::<u8>(&train_path) {
-    //     Ok(_) => println!("Model trained."),
-    //     Err(_) => println!("Model training failed.")
-    // };
-
-    // match model.test::<u8>(&test_path) {
-    //     Ok(accuracy) => println!("Model accuracy: {}", accuracy),
-    //     Err(_) => println!("Model testing failed.")
-    // };
-
-    // model.to_json(&String::from("./models/balanced/balanced.json"));
 }
 
 fn main() {
